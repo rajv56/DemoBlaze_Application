2bbb0f0bdd61789f7cfa6a2f3072f071b80ee724
Raj <rajeshjavarpa@gmail.com>
2025-12-13 07:58:15 +0530

    First Commit

---
+++ pages/Sign_Up.js
@@ -0,0 +1,20 @@
+export   class Signup {
+
+//Locators :   
+
+constructor{
+
+//username , password , signup button, close button and signup and close icon
+
+ }
+
+
+Method : doSignup () {
+	username.fill()
+	password.fill()
+	click on signup button
+}
+
+
+}
+
