7d794972792b9e8a7153672853fab1398a2b6f7f
Raj <rajeshjavarpa@gmail.com>
2025-12-13 08:42:04 +0530

    LoginPage

--- pages/Login_Page.js
+++ pages/Login_Page.js
@@ -1,5 +1,23 @@
-class LoginPage(){
+import { expect } from '@playwright/test'
+
+class LoginPage {
+
+    constructor(page) {
+        this.page = page;
+        this.loginLabel = page.locator('#logInModalLabel');
+        this.userName = page.locator('#loginusername');
+        this.passWord = page.locator('#loginpassword');
+        this.loginButton = page.locator(`onclick=logIn`);
+    }
+
+    async doLogin() {
+        //write an assertion for login label to verify login window open
+        expect(this.loginLabel).toBeVisible();
+        await this.userName.fill('pavanol');
+        await this.passWord.fill('test@123');
+        await this.loginButton.click();
+
+    }
 
 
-    
 }
